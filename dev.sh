e56db5a0979cacdc9b28042a1ab113a320a5b60a
vkobinski <victorkobinski@outlook.com>
2024-09-20 23:16:35 -0300

    First

--- dev.sh
+++ dev.sh
@@ -4,5 +4,5 @@ IFS=$'\n\t'
 
 (trap 'kill 0' SIGINT; \
  bash -c 'cargo install trunk' & \
- bash -c 'cd frontend; CARGO_TARGET_DIR=../target-trunk trunk build --address 0.0.0.0' & \
- bash -c 'cd server; cargo watch -- cargo run')
+ bash -c 'cd frontend; trunk build --address 0.0.0.0' & \
+ bash -c 'cargo run --release')
