e00a4cce5514b748998aac9e54776774d4ba0d4c
vkobinski <victorkobinski@outlook.com>
2024-09-20 23:21:26 -0300

    First

--- dev.sh
+++ dev.sh
@@ -4,5 +4,7 @@ IFS=$'\n\t'
 
 (trap 'kill 0' SIGINT; \
  bash -c 'cargo install trunk' & \
+ bash -c 'cargo install sqlx-cli' & \
+ bash -c 'cargo sqlx database create' & \
  bash -c 'cd frontend; trunk build --address 0.0.0.0' & \
  bash -c 'cargo run --release')
